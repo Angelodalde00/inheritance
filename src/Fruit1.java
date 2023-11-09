96364df274d733c0f24a7f343b0ab6d012d5ddf1
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-09 18:22:51 +0800

    Second commit Fruit1

---
+++ src/Fruit1.java
@@ -0,0 +1,17 @@
+public class Fruit1 {
+    
+    String name;
+    
+    Fruit1 (String name){
+        
+    }
+    void kaon(){
+        System.out.println(name + "gi kaon");
+        
+    }
+    void tulon(){
+        System.out.println(name + "gi tulon");
+    }
+    
+}
+
