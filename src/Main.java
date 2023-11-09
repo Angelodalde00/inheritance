1fd1e3db67e68c238b4d8d4982ccabf0458f1afe
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-09 18:21:24 +0800

    first commit Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  
+        public static void main(String[] args){
+            
+            Santol mySantol = new Santol("Lester");
+            
+            mySantol.kaon();
+            mySantol.tulon();
+            
+            
+        }
+        
+        
+        
+}
